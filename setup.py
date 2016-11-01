c43b7c1c766e1d5f863e65e8867d75976d1b083c
mworion <michael.wuertenberger@t-online.de>
2016-11-01 23:14:06 +0100

    corrected about directories to be installed

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from distutils.core import setup
 
 setup(
     name='mountwizzard',
-    version='0.13',
+    version='0.16',
     packages=[
         'mountwizzard',
         'mountwizzard/support',
     ],
+    package_data={'mountwizzard': ['config/*', 'analysedata/*', 'images/*']},
+    include_package_data=True,
     url='https://pypi.python.org/pypi/mountwizzard',
     license='APL 2.0',
     author='mw',
