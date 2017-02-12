5a26ffe09be508545fbad055a87b3d392361030e
mworion <michael.wuertenberger@t-online.de>
2017-02-12 15:06:35 +0100

    update widget signal processing

--- mountwizzard/mountwizzard.py
+++ mountwizzard/mountwizzard.py
@@ -797,6 +797,7 @@ class MountWizzardApp(MwWidget):
                 self.coordinatePopup.ui.modellingLog.setText(self.coordinatePopup.ui.modellingLog.toPlainText() + text)     # otherwise add text at the end
             self.coordinatePopup.ui.modellingLog.moveCursor(QTextCursor.End)                                                # and move cursor up
             self.modelLogQueue.task_done()
+        QGuiApplication.processEvents()
         # noinspection PyCallByClass,PyTypeChecker
         QTimer.singleShot(200, self.mainLoop)                                                                               # 200ms repeat time cyclic
 
