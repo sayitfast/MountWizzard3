8cf1790fcc9f8fd71922298826245f1144bed0c6
mworion <michael.wuertenberger@t-online.de>
2017-03-12 11:50:38 +0100

    updating todo's in theskyx module

--- mountwizzard/support/theskyx.py
+++ mountwizzard/support/theskyx.py
@@ -41,8 +41,8 @@ class TheSkyX:
         finally:
             tsxSocket.close()
             if connected:
-                suc, mes = self.SgGetDeviceStatus('Camera')
-                if suc and mes != 'DISCONNECTED':
+                success, response = self.SgGetDeviceStatus('Camera')
+                if success and response != 'DISCONNECTED':
                     if self.SgGetDeviceStatus('PlateSolver'):
                         return True, 'Camera and Solver OK'
                     else:
@@ -53,10 +53,15 @@ class TheSkyX:
 
     @staticmethod
     def SgEnumerateDevice(self, device):
+        # TODO: implement
         return '', False, 'Not implemented'
 
     def SgCaptureImage(self, binningMode=1, exposureLength=1, gain=None, iso=None, speed=None, frameType=None,
                        filename=None, path=None, useSubframe=False, posX=0, posY=0, width=1, height=1):
+        # TODO: is there any chance to set filename in script to TSX ?
+        # TODO: or is there a way to set at least the sequence number ?
+        # TODO: how is TSX dealing with ISO settings for DSLR?
+        # TODO: how is TSX dealing with download speeds for CCD, who support this feature ?
         if frameType == 'Light':
             frameType = 'cdLight'
         try:
@@ -84,16 +89,17 @@ class TheSkyX:
             return False, 'Request failed', ''
 
     def SgSolveImage(self, path, raHint=None, decHint=None, scaleHint=None, blindSolve=False, useFitsHeaders=False):
+        # TODO: implementation with blind fail over (if it is possible to enable / disable in TSX)
         try:
             command = '/* Java Script */'
             command += 'ccdsoftCamera.Asynchronous=1;'
             command += 'ImageLink.pathToFITS="' + path.replace('\\', '/') + '";'
-            if scaleHint == None:
-                command += 'ImageLink.unknownScale=1;'
-                command += 'ImageLink.scale=2;'
-            else:
+            if scaleHint:
                 command += 'ImageLink.unknownScale=0;'
                 command += 'ImageLink.scale=' + str(scaleHint) + ';'
+            else:
+                command += 'ImageLink.unknownScale=1;'
+                command += 'ImageLink.scale=2;'
             command += 'ImageLink.execute();'
             command += 'var Out = "";'
             command += 'Out=ImageLinkResults.succeeded'
@@ -103,33 +109,42 @@ class TheSkyX:
             self.logger.error('TheSkyX SgSolveImage -> error: {0}'.format(e))
             return False, 'Request failed'
 
-    def SgGetSolvedImageData(self, guid):
+    def SgGetSolvedImageData(self, guid=None):
         try:
-            command = '/* Java Script */'
-            command += 'ccdsoftCamera.Asynchronous=0;'
-            command += 'var Out = "";'
-            command += 'Out=String(\'{"succeeded":"\'+ImageLinkResults.succeeded+\'","imageCenterRAJ2000":"\'+ImageLinkResults.imageCenterRAJ2000+\'","imageCenterDecJ2000":"\'+ImageLinkResults.imageCenterDecJ2000+\'","imageScale":"\'+ImageLinkResults.imageScale+\'","imagePositionAngle":"\'+ImageLinkResults.imagePositionAngle+\'"}\');'
-            success, response = self.sendCommand(command)
-            if success:
-                captureResponse = json.loads(response)
-                if captureResponse['succeeded'] == '1':
-                    success = True
+            if guid:
+                command = '/* Java Script */'
+                command += 'ccdsoftCamera.Asynchronous=0;'
+                command += 'var Out = "";'
+                command += 'Out=String(\'{"succeeded":"\'+ImageLinkResults.succeeded+\'","imageCenterRAJ2000":"\'+ImageLinkResults.imageCenterRAJ2000+\'","imageCenterDecJ2000":"\'+ImageLinkResults.imageCenterDecJ2000+\'","imageScale":"\'+ImageLinkResults.imageScale+\'","imagePositionAngle":"\'+ImageLinkResults.imagePositionAngle+\'"}\');'
+                success, response = self.sendCommand(command)
+                if success:
+                    captureResponse = json.loads(response)
+                    if captureResponse['succeeded'] == '1':
+                        success = True
+                    else:
+                        success = False
+                    return success, 'succeeded', captureResponse['imageCenterRAJ2000'], captureResponse['imageCenterDecJ2000'], captureResponse['imageScale'], captureResponse['imagePositionAngle'], '1'
                 else:
-                    success = False
-                return success, 'succeeded', captureResponse['imageCenterRAJ2000'], captureResponse['imageCenterDecJ2000'], captureResponse['imageScale'], captureResponse['imagePositionAngle'], '1'
+                    return False, 'Request failed', '', '', '', '', ''
             else:
-                return False, 'Request failed', '', '', '', '', ''
+                self.logger.error('TXGetSolvedImag-> no GUID')
+                return False, 'No GUID'
         except Exception as e:
             self.logger.error('TXGetSolvedImag-> error: {0}'.format(e))
             return False, 'Request failed', '', '', '', '', ''
 
-    def SgGetImagePath(self, guid):
+    def SgGetImagePath(self, guid=None):
+        # TODO: as TSX don't use references, but the latest one, is there any chance to ensure sequence (I did it with asynchronous = 0 in all use cases)
         try:
-            command = '/* Java Script */'
-            command += 'var Out = "";'
-            command += 'Out=ccdsoftCamera.LastImageFileName;'
-            success, response = self.sendCommand(command)
-            return success, response
+            if guid:
+                command = '/* Java Script */'
+                command += 'var Out = "";'
+                command += 'Out=ccdsoftCamera.LastImageFileName;'
+                success, response = self.sendCommand(command)
+                return success, response
+            else:
+                self.logger.error('TXGetImagePath -> no GUID')
+                return False, 'No GUID'
         except Exception as e:
             self.logger.error('TXGetImagePath -> error: {0}'.format(e))
             return False, 'Request failed'
@@ -159,6 +174,8 @@ class TheSkyX:
             return False, 'Device has no status'
 
     def SgGetCameraProps(self):
+        # TODO: Get the chance to implement subframe on / off if a camera doesn't support this
+        # TODO: Gain setting in CMOS Cameras necessary ?
         try:
             command = '/* Java Script */'
             command += 'var Out = "";'
