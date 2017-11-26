4a6ad6985b223425bf89f28d2cf62d29a7f3e0b7
mworion <michael.wuertenberger@t-online.de>
2017-11-26 18:42:36 +0100

    bugfix delete worst point if not data set is there

---
+++ mountwizzard/gui/message_dialog_ui.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'message_dialog_ui.ui'
+#
+# Created by: PyQt5 UI code generator 5.9
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MessageDialog(object):
+    def setupUi(self, MessageDialog):
+        MessageDialog.setObjectName("MessageDialog")
+        MessageDialog.resize(791, 639)
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        MessageDialog.setFont(font)
+        self.messages = QtWidgets.QTextBrowser(MessageDialog)
+        self.messages.setGeometry(QtCore.QRect(10, 10, 771, 621))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.messages.sizePolicy().hasHeightForWidth())
+        self.messages.setSizePolicy(sizePolicy)
+        self.messages.setMinimumSize(QtCore.QSize(0, 100))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(10)
+        self.messages.setFont(font)
+        self.messages.setFrameShadow(QtWidgets.QFrame.Plain)
+        self.messages.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
+        self.messages.setAcceptRichText(False)
+        self.messages.setObjectName("messages")
+
+        self.retranslateUi(MessageDialog)
+        QtCore.QMetaObject.connectSlotsByName(MessageDialog)
+
+    def retranslateUi(self, MessageDialog):
+        _translate = QtCore.QCoreApplication.translate
+        MessageDialog.setWindowTitle(_translate("MessageDialog", "Messages"))
+        self.messages.setToolTip(_translate("MessageDialog", "Error Messages from Tool"))
+        self.messages.setHtml(_translate("MessageDialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Arial\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'MS Shell Dlg 2\'; font-size:8pt;\"><br /></p></body></html>"))
+
