eeba445becc0d44463b8cc47a52c65204537f1c7
mworion <michael.wuertenberger@t-online.de>
2018-03-17 19:12:05 +0100

    optimized gui by splitting high load and low load parts into two widgets

--- mountwizzard/widgets/hemisphereWindow.py
+++ mountwizzard/widgets/hemisphereWindow.py
@@ -47,6 +47,7 @@ class HemisphereWindow(widget.MwWidget):
         self.pointerTrack = None
         self.pointsPlotBig = None
         self.pointsPlotSmall = None
+        self.pointsPlotCross = None
         self.maskPlotFill = None
         self.maskPlotMarker = None
         self.annotate = list()
@@ -164,7 +165,7 @@ class HemisphereWindow(widget.MwWidget):
             self.drawCanvasMoving()
 
     def plotImagedPoint(self, az, alt):
-        self.hemisphereMatplotlib.axes.plot(az, alt, 'X', color='#FF00FF', zorder=5, markersize=9)
+        self.pointsPlotCross.set_data(numpy.append(az, self.pointsPlotCross.get_xdata()), numpy.append(alt, self.pointsPlotCross.get_ydata()))
         self.drawCanvas()
 
     def setEditModus(self):
@@ -325,6 +326,8 @@ class HemisphereWindow(widget.MwWidget):
         # add text to points
         for i in range(0, len(points)):
             self.annotate.append(self.hemisphereMatplotlib.axes.annotate('{0:2d}'.format(i+1), xy=(points[i][0] - self.offx, points[i][1] - self.offy), color='#E0E0E0', picker='None'))
+        # add crosses, if modeling was done to recap when opening the window
+        self.pointsPlotCross, = self.hemisphereMatplotlib.axes.plot([], [], 'X', color='#FF00FF', zorder=5, markersize=9)
         # now to the second widget on top of the first one
         # adding the pointer of mount to hemisphereMoving plot
         self.pointerAzAlt1,  = self.hemisphereMatplotlibMoving.axes.plot(180, 45, zorder=10, color='#FF00FF', marker='o', markersize=25, markeredgewidth=3, fillstyle='none', visible=False, picker='None')
