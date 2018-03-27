388b435a16bc898263fad57dc5104a65e95b0cfb
mworion <antrieb69>
2018-03-27 20:54:50 +0200

    optimized order for grid as well

--- mountwizzard/modeling/modelingPoints.py
+++ mountwizzard/modeling/modelingPoints.py
@@ -436,9 +436,15 @@ class ModelPoints:
 
     def generateGridPoints(self, limitByHorizonMask, doSortingPoints, numberOfRows, numberOfColumns, altitudeMin, altitudeMax):
         self.modelPoints = list()
-        for az in range(5, 360, int(360 / numberOfColumns)):
-            for alt in range(altitudeMin, altitudeMax + 1, int((altitudeMax - altitudeMin) / (numberOfRows - 1))):
-                self.modelPoints.append((az, alt))
+        i = 0
+        for alt in range(altitudeMin, altitudeMax + 1, int((altitudeMax - altitudeMin) / (numberOfRows - 1))):
+            if i % 2:
+                for az in range(365 - int(360 / numberOfColumns), 0, -int(360 / numberOfColumns)):
+                    self.modelPoints.append((az, alt))
+            else:
+                for az in range(5, 360, int(360 / numberOfColumns)):
+                    self.modelPoints.append((az, alt))
+            i += 1
         if limitByHorizonMask:
             self.deleteBelowHorizonLine()
         if doSortingPoints:
