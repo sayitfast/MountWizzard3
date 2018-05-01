19ca629529fe7884e0d0338de456f546b7c69162
mworion <michael.wuertenberger@t-online.de>
2018-05-01 18:36:30 +0200

    bugfix warning tone

--- mountwizzard/mount/mount_statusmedium.py
+++ mountwizzard/mount/mount_statusmedium.py
@@ -213,7 +213,8 @@ class MountStatusRunnerMedium(PyQt5.QtCore.QObject):
                     self.data['RefractionTemperature'] = valueList[4]
                 if len(valueList[5]) > 0:
                     self.data['RefractionPressure'] = valueList[5]
-                self.app.workerMountDispatcher.signalMountLimits.emit(self.data['MeridianLimitGuide'], self.data['MeridianLimitSlew'])
+                if 'MeridianLimitGuide' in self.data and 'MeridianLimitSlew' in self.data:
+                    self.app.workerMountDispatcher.signalMountLimits.emit(self.data['MeridianLimitGuide'], self.data['MeridianLimitSlew'])
             else:
                 self.logger.warning('Parsing Status Medium combined command valueList is not OK: length:{0} content:{1}'.format(len(valueList), valueList))
         except Exception as e:
