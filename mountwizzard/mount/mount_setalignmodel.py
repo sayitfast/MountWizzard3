e00e539155824cd30ca6e9e8255ec79aa71b7e57
mworion <michael.wuertenberger@t-online.de>
2018-05-05 20:58:18 +0200

    bugfix counting points

--- mountwizzard/mount/mount_setalignmodel.py
+++ mountwizzard/mount/mount_setalignmodel.py
@@ -164,7 +164,7 @@ class MountSetAlignmentModel(PyQt5.QtCore.QObject):
         # now we got all information about the model write run
         valueList = messageToProcess.strip('#').split('#')
         # quick check:
-        if len(valueList) != self.numberAlignmentPoints:
+        if len(valueList) != self.numberAlignmentPoints + 1:
             # error happened
             self.logger.error('Parsing SetAlignmentModel wrong numbers: value:{0}, points:{1}, values:{2}'.format(len(valueList), self.numberAlignmentPoints, valueList))
         # now parsing the result
