3cafd695f0f51924df31a15fef6f46a24a78336f
mworion <michael.wuertenberger@t-online.de>
2018-06-14 19:42:42 +0200

    beta7dev0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import platform
 
 setup(
     name='mountwizzard3',
-    version='3.0b6',
+    version='3.0b7.dev0',
     packages=[
         'mountwizzard3',
         'mountwizzard3.analyse',
--- setup_arm.py
+++ setup_arm.py
@@ -4,7 +4,7 @@ import platform
 
 setup(
     name='mountwizzard3_ARM',
-    version='3.0b6',
+    version='3.0b7.dev0',
     packages=[
         'mountwizzard3',
         'mountwizzard3.analyse',
