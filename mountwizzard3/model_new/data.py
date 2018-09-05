048bc93b301c5881e70c2521076c8ea153181cab
mworion <michael.wuertenberger@t-online.de>
2018-09-05 19:09:37 +0200

    first implementation data class model

--- mountwizzard3/model_new/data.py
+++ mountwizzard3/model_new/data.py
@@ -76,10 +76,10 @@ class Build(object):
     """
 
     __all__ = ['Build',
-               'starList',
-               'addStar',
-               'delStar',
-               'checkStarListOK',
+               'setList',
+               'addSet',
+               'delSet',
+               'checkSetListOK',
                ]
     version = '0.1'
     logger = logging.getLogger(__name__)
@@ -87,19 +87,18 @@ class Build(object):
     def __init__(self,
                  ):
 
-        self._starList = list()
-
+        self._setList = list()
 
     @property
-    def starList(self):
-        return self._starList
+    def setList(self):
+        return self._setList
 
-    @starList.setter
-    def starList(self, value):
+    @setList.setter
+    def setList(self, value):
         if isinstance(value, list) and len(value) > 0:
-            self._starList = value
+            self._setList = value
         else:
-            self._starList = list()
+            self._setList = list()
 
     def addStar(self, value):
         """
@@ -110,7 +109,7 @@ class Build(object):
         """
 
         if isinstance(value, BuildStar):
-            self._starList.insert(len(self._starList), value)
+            self._setList.insert(len(self._setList), value)
             return
         if not isinstance(value, (list, str)):
             self.logger.error('malformed value: {0}'.format(value))
@@ -123,7 +122,7 @@ class Build(object):
                               errorRMS=_err,
                               errorAngle=_angle,
                               number=_number)
-            self._starList.insert(len(self._starList), value)
+            self._setList.insert(len(self._setList), value)
 
     def delStar(self, value):
         """
@@ -137,12 +136,12 @@ class Build(object):
         except Exception as e:
             self.logger.error('error: {0}, malformed value: {1}'.format(e, value))
             return
-        if value < 0 or value > len(self._starList) - 1:
+        if value < 0 or value > len(self._setList) - 1:
             self.logger.error('invalid value: {0}'.format(value))
             return
-        self._starList.pop(value)
+        self._setList.pop(value)
 
-    def checkStarListOK(self):
+    def checkSetListOK(self):
         """
         Make a check if the actual alignment star count by polling gets the same
         number of stars compared to the number of stars in the list.
@@ -150,7 +149,7 @@ class Build(object):
 
         :return: True if same size
         """
-        if self._numberStars == len(self._starList):
+        if self._numberStars == len(self._setList):
             return True
         else:
             return False
@@ -158,34 +157,24 @@ class Build(object):
 
 class BuildStar(object):
     """
-    The class ModelStar inherits all informations and handling of one star in
-    the alignment model used by the mount and the data in the mount and provides the
-    abstracted interface to a 10 micron mount.
-    The coordinates are in JNow topocentric
+    The class BuildStar inherits all informations and handling of the coordinates of
+    a pointing direction of the nominal mount coordinates and the plate solved
+    coordinates, which were derived from a taken image at the scope
+    the coordinates both are in JNow topocentric
 
         >>> settings = BuildStar(
         >>>                     )
 
-    point could be from type skyfield.api.Star or just a tuple of (ha, dec) where
+    coordinates could be from type skyfield.api.Star or just a tuple of (ha, dec) where
     the format should be float or the 10micron string format.
 
-    Command protocol (from2.8.15 onwards):
-    "HH:MM:SS.SS,+dd*mm:ss.s,eeee.e,ppp#" where HH:MM:SS.SS is the hour angle of the
-    alignment star in hours, minutes, seconds and hundredths of second (from 0h to
-    23h59m59.99s), +dd*mm:ss.s is the declination of the alignment star in degrees,
-    arcminutes, arcseconds and tenths of arcsecond, eeee.e is the error between the star
-    and the alignment model in arcseconds, ppp is the polar angle of the measured star
-    with respect to the modeled star in the equatorial system in degrees from 0 to 359
-    (0 towards the north pole, 90 towards east).
     """
 
     __all__ = ['ModelStar',
-               'point',
-               'errorRMS',
-               'errorAngle',
-               'errorRA',
-               'errorDEC',
-               'number',
+               'mountStar',
+               'pierside',
+               'solveStar',
+               'sidereal',
                ]
     version = '0.1'
     logger = logging.getLogger(__name__)
@@ -264,7 +253,6 @@ class BuildStar(object):
                                                 dec_degrees=_dec)
         else:
             self.logger.error('malformed value: {0}'.format(value))
-
 
     @property
     def sidereal(self):
