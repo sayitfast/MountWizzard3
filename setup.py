392894030f5dae1c3b3a8d4c566dac6bbd4e6ab2
mworion <michael.wuertenberger@t-online.de>
2018-12-15 19:23:42 +0100

    update packages

--- setup.py
+++ setup.py
@@ -30,11 +30,11 @@ setup(
     python_requires='~=3.6.3',
     install_requires=[
         'PyQt5==5.11.3',
-        'matplotlib==3.0.1',
-        'wakeonlan>=1.0.0',
-        'requests==2.20.0',
-        'astropy==3.0.3',
-        'numpy==1.15.3',
+        'matplotlib==3.0.2',
+        'wakeonlan==1.1.6',
+        'requests==2.21.0',
+        'astropy==3.1',
+        'numpy==1.15.4',
         'requests_toolbelt==0.8.0'
     ]
     + (['pypiwin32==220'] if "Windows" == platform.system() else [])
